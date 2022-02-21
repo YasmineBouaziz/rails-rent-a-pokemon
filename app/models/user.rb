a3c34f10577b7758a4e45f4da33f9654edf33b6c
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-02-21 15:38:35 +0000

    added email validation to the user and also added a validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, :email, :password, presence: true
   validates :name, :email, :password, uniqueness: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
   has_many :pokemons, :bookings
+  has_many :reviews, through: :bookings
 end
