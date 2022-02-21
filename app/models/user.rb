d4130d87c5001779c2b14d1e057c481faa6a84d4
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-02-21 15:29:48 +0000

    added user model, migrated and added validations to the user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :name, :email, :password, presence: true
+  validates :name, :email, :password, uniqueness: true
+  has_many :pokemons, :bookings
+end
