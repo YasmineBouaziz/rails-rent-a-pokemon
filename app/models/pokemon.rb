715d247c1eb46506c228bb76a25dd57be6579a41
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-02-22 14:34:44 +0000

    displayed pokemons in controller and in the view, now able to see the name of pokemon selected and the details

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon < ApplicationRecord
   validates :name, :description, :photo_url, presence: true
-  validates :name, uniqueness: true
+  #validates :name, uniqueness: true
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
