696a1627a0cf6b49ca0e46414d3c69422db01ace
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-02-22 12:58:38 +0000

    added ability to create pokemon, but needed to edit a lot of user validations that now need sorting out

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,11 +1,22 @@
 class PokemonsController < ApplicationController
-def new
-  @pokemon = Pokemon.new
-end
+  def new
+    @pokemon = Pokemon.new
+  end
 
-def create
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.user_id = current_user.id
 
-end
+    if @pokemon.save
+      redirect_to @pokemon, notice: "#{@pokemon.name} was successfully created."
+    else
+      render :new, notice: "Something went wrong. Try again."
+    end
+  end
 
+  private
 
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :description, :photo_url, :price)
+  end
 end
