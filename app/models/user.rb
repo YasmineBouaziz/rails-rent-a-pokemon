234f74696b016115c4dd11bf735cddabf0c1d9bc
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-02-22 17:26:27 +0000

    displayed bookings and fixed the navbar to fit whole screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, :email, :password, presence: true
-  validates :email, uniqueness: true #removed name and password
+  # validates :name, :email, :password, presence: true
+  # validates :email, uniqueness: true #removed name and password
   # validates :email, format: { with: /\A.*@.*\.com\z/ }
   has_many :pokemons
+  has_many :bookings
   has_many :reviews, through: :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pokemons do
-    resources :bookings
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :destroy, :update] do
+    resources :reviews, only: [:create, :index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
