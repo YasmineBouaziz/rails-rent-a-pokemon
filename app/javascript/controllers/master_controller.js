b0644130ff7c9ec4527fbac54ccc72050f36768d
phillip burns <phillipworld@hotmail.co.uk>
2022-02-24 10:39:58 +0000

    Further adjustments

--- app/javascript/controllers/master_controller.js
+++ app/javascript/controllers/master_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    console.log("Works")
   }
 }
