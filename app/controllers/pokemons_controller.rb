2c44e231186119e5d8b677029074ed630f20e88b
Natalija Stepurko <natalija.stepurko@gmail.com>
2022-02-25 13:17:29 +0000

    Pokemons with no user images will have images from API

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -25,6 +25,12 @@ class PokemonsController < ApplicationController
   def create
     # To write if statement to fetch a picture from API, if no photos given by the user
     @pokemon = Pokemon.new(pokemon_params)
+    if Pokemon.where(photos: [])
+      # @pokemon.photos.present?
+      get_pokemon_photo(@pokemon.name)
+    else
+      @pokemon = Pokemon.new(pokemon_params)
+    end
     @pokemon.user_id = current_user.id
 
     if @pokemon.save
@@ -38,5 +44,16 @@ class PokemonsController < ApplicationController
 
   def pokemon_params
     params.require(:pokemon).permit(:name, :description, :price, :photo_url, photos: [])
+  end
+
+  def get_pokemon_photo(name)
+    url = "https://pokeapi.co/api/v2/pokemon/#{name}"
+    response = RestClient.get(url)
+    pokemon_image = JSON.parse(response)["sprites"]["other"]["official-artwork"]["front_default"]
+    if pokemon_image.present?
+      @pokemon.photo_url = pokemon_image
+    else
+      fetcher(PokemonList::LIST.sample)
+    end
   end
 end
